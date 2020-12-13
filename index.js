3d99f80f638d09d98c29f330ecb3989d9700d03b
februar-y <jarmah241@gmail.com>
2020-12-13 13:38:16 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+// move left
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight () {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0 & left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+// move right
+
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+})
